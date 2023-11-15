04310639ecda95a87a3c30e51faa013839a42e38
Ignacio-amat <amat0002@hz.nl>
2023-11-15 13:47:07 +0100

    First backend with apigateway and events microservice

---
+++ apigateway/code/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+const router = express.Router();
+
+// create a proxy for each microservice
+const eventProxy = createProxyMiddleware({
+  target: 'http://events:3010',
+  changeOrigin: true,
+});
+
+router.use('/events', cors(), eventProxy);
+
+
+export default router;
---
+++ events/code/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+export function ErrorHandler(err, req, res, next) {
+  console.log('Middleware Error Handling');
+  const errStatus = err.statusCode || 500;
+  const errMsg = err.message || 'Something went wrong';
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMsg,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : {},
+  });
+}
