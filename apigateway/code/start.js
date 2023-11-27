cbf6dee80ddb6faa80e04a8f9bd78d38dddcc8e0
vladpavlovhz <pavl0008@hz.nl>
2023-11-27 13:41:48 +0100

    error-search: Changed routes, error now in the right place

--- apigateway/code/start.js
+++ apigateway/code/start.js
@@ -1,16 +1,16 @@
-// start.js setup from learnnode.com by Wes Bos
 import express from 'express';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: 'variables.env' });
-
-import indexRouter from './routes/index.js';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+import indexRouter from './routes/index.js';
+
+dotenv.config({ path: 'variables.env' });
 
 const app = express();
 
-// support json encoded and url-encoded bodies, mainly used for post and update
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 
--- events/code/index.js
+++ events/code/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/events', eventsRouter);
+app.use('/', eventsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
