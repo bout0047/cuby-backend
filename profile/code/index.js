aa4552a548c3563e11613b2c11eb007790f1862f
vladpavlovhz <pavl0008@hz.nl>
2024-01-17 10:41:46 +0100

    import router fixes

--- profile/code/index.js
+++ profile/code/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import pool from './db/index.js';
-import profileRouter from './routes/profile';
+import profileRouter from './routes/profile.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
