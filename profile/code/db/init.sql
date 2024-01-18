a54cf1787493a1db2437a482ef21a5ce33f2224d
Ignacio-amat <amat0002@hz.nl>
2024-01-18 19:04:02 +0100

    fixed slight error when seeding profiles

--- profile/code/db/init.sql
+++ profile/code/db/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS profile (
         id SERIAL PRIMARY KEY,
-        user_id INTEGER UNIQUE not null,
+        user_id INTEGER UNIQUE,
         name VARCHAR(255) UNIQUE not null,
         email VARCHAR(255) UNIQUE not null,
         goals text[],
