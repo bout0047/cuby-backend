8378d49433f41aea16956f0d54188c9a211a369a
vladpavlovhz <pavl0008@hz.nl>
2024-01-24 13:05:46 +0100

    unit-tests-profile

--- events/code/scraper.js
+++ events/code/scraper.js
@@ -2,9 +2,9 @@ import cheerio from 'cheerio';
 import axios from 'axios';
 import isOnline from 'is-online';
 import striptags from 'striptags';
- 
+
 const cleanHTML = (html) => striptags(html);
- 
+
 const scrapeEvents = async () => new Promise(async (resolve, reject) => {
   try {
     const online = await isOnline();
@@ -12,7 +12,7 @@ const scrapeEvents = async () => new Promise(async (resolve, reject) => {
       console.log('No internet connection. Skipping scraping.');
       return resolve([]);
     }
- 
+
     const url = 'https://allevents.in/events?ref=cpt';
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
@@ -24,9 +24,9 @@ const scrapeEvents = async () => new Promise(async (resolve, reject) => {
       const eventPlaceScraped = eventMetaIScraped.find('.up-venue').text().trim();
       const eventDateScrapedRaw = eventDateDisplayScraped ? eventDateDisplayScraped.text() : '';
       const eventDateScraped = eventDateScrapedRaw.replace(/\s+/g, ' ').trim();
- 
+
       const eventTitleScraped = eventMetaIIScraped.text().replace(/\s+/g, ' ').trim();
- 
+
       // Scrape event description and image
       const eventLink = $(element).data('link');
       const eventResponse = await axios.get(eventLink);
@@ -40,37 +40,36 @@ const scrapeEvents = async () => new Promise(async (resolve, reject) => {
         eventDescription: eventDescription.trim(),
         eventImage: eventImage ? eventImage.trim() : undefined,
       };
- 
+
       return eventData;
     }).get();
- 
+
     const events = await Promise.all(eventPromises);
     resolve(events);
   } catch (error) {
     reject(error);
   }
 });
- 
+
 const startScrapingWithInterval = () => {
   const scrapeEvery3Hours = async () => {
     try {
       const online = await isOnline();
       if (!online) {
         console.log('No internet connection. Skipping scraping.');
-        return;
       }
       // Now you can do whatever you want with the events array
     } catch (error) {
       console.error('Error:', error);
     }
   };
- 
+
   scrapeEvery3Hours();
- 
+
   const interval = 3 * 60 * 60 * 1000; // 3 hours
   setInterval(scrapeEvery3Hours, interval);
 };
- 
+
 startScrapingWithInterval();
- 
+
 export default scrapeEvents;
